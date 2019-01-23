8c2b13c9a9b75fc228e1a805c942411912405a01
Andrew Craigie <a.craigie@btinternet.com>
2019-01-23 12:05:09 +0000

    Adds NumberToWordsTestTens. Test passes.

---
+++ test/NumberToWordsTestTens.java
@@ -0,0 +1,45 @@
+import java.util.Arrays;
+import java.util.Collection;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameters;
+
+import static org.junit.Assert.assertEquals;
+
+@RunWith(Parameterized.class)
+public class NumberToWordsTestTens {
+
+
+    @Parameters
+    public static Collection<Object[]> tensData() {
+        return Arrays.asList(new Object[][]{
+                {0, ""},
+                {1, ""},
+                {2, "Twenty"},
+                {3, "Thirty"},
+                {4, "Forty"},
+                {5, "Fifty"},
+                {6, "Sixty"},
+                {7, "Seventy"},
+                {8, "Eighty"},
+                {9, "Ninety"}
+        });
+    }
+
+    private int numberInput;
+    private String wordOutput;
+
+
+    public NumberToWordsTestTens(int numberInput, String wordOutput) {
+        this.numberInput = numberInput;
+        this.wordOutput = wordOutput;
+    }
+
+    @Test
+    public void testNumberToWordsUnitsStrings() {
+        assertEquals(wordOutput, NumberToWords.tens[numberInput]);
+    }
+
+}
